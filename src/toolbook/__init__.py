81cccbc090712890c8bb10cde723a7f5cd856a89
GAURAV KADAM <gskadam3b@gmail.com>
2026-06-06 14:56:44 +0530

    auther details

--- src/toolbook/__init__.py
+++ src/toolbook/__init__.py
@@ -1,3 +1,5 @@
 __version__ = "0.0.1"
-
-__all__ = ["__version__"]
+# my_library/__init__.py
+__author__ = "Gaurav Kadam"
+__email__ = "gskadam3b@gmail.com"
+__all__ = ["__version__","__author__","__email__"]
